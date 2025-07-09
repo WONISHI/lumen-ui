0cbb3ce2ee5a10779267904afe6b0c10e43844b1
weizhifeng <18797942292@139.com>
2025-07-09 22:55:04 +0800

    feat:新增测试用例

---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
+// @ts-ignore
+import eslint from "vite-plugin-eslint";
+import VueMacros from "unplugin-vue-macros";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    VueMacros.vite({
+      plugins: {
+        vue: vue(),
+        vueJsx: vueJsx(),
+      },
+    }),
+    eslint(),
+    vueDevTools(),
+  ],
+  test:{
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
