9267b6b0993cd7cddcc37203194c654f78f03d29
weizhifeng <18797942292@139.com>
2025-07-19 16:47:00 +0800

    feat:添加message的测试用例

--- src/components/Message/Message.test.tsx
+++ src/components/Message/Message.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 import { nextTick } from "vue";
 import { createMessage, closeAll } from "./methods";
 
