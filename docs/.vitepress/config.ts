ca427f8111bdf0d7ecdc3cd0f9d5f195d7b51eac
weizhifeng <18797942292@139.com>
2025-07-22 22:59:16 +0800

    初步添加input组件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -53,6 +53,6 @@ export default defineConfig({
       },
     ],
 
-    socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+    socialLinks: [{ icon: "github", link: "https://github.com/WONISHI/lumen-ui" }],
   },
 });
---
+++ src/components/Input/Input.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="lu-input"></div>
+</template>
+<script lang="ts" setup>
+import type { InputProps } from "./types";
+defineProps<InputProps>();
+</script>
+<style></style>
