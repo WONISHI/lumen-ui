dae0884df88d2537da2c6410411f42d077ec7363
weizhifeng <18797942292@139.com>
2025-07-26 18:37:10 +0800

    feat:添加switch组件

---
+++ src/components/Switch/Swich.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lu-switch"></div>
+</template>
+<script lang="ts" setup>
+import type { SwitchProps, SwitchEmits } from "./types";
+import { ref, computed } from "vue";
+defineOptions({
+  name: "LuSwitch",
+  inheritAttrs: false,
+});
+const props = defineProps<SwitchProps>();
+const emits = defineEmits<SwitchEmits>();
+const innerValue = ref(props.modelValue);
+const checked = computed(() => innerValue.value);
+</script>
