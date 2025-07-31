d1a6d1c30f59b6e5fbc47d6f4b57a51fbe28a4f4
weizhifeng <18797942292@139.com>
2025-07-31 22:04:09 +0800

    feat:新增配置项

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -34,6 +34,17 @@ export default defineConfig({
     lineNumbers: true,
   },
   themeConfig: {
+    logo: "/logo.svg",
+    outline: { label: "目录", level: [2, 6] },
+    // 主题
+    darkModeSwitchLabel: "深浅模式",
+    // 返回顶部label
+    returnToTopLabel: "返回顶部",
+    //自定义上下页名
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "首页", link: "/" },
@@ -66,5 +77,24 @@ export default defineConfig({
     ],
 
     socialLinks: [{ icon: "github", link: "https://github.com/WONISHI/lumen-ui" }],
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    }
   },
 });
