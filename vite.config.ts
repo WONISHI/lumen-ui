e68182b5c697c6a2846548c33396047f0245fa2e
weizhifeng <18797942292@139.com>
2025-08-06 21:16:03 +0800

    feat:新增rollup配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // @ts-ignore
 import eslint from "vite-plugin-eslint";
 import VueMacros from "unplugin-vue-macros";
+import { visualizer } from "rollup-plugin-visualizer";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,10 +23,32 @@ export default defineConfig({
       include: ["src/**/*.ts", "src/**/*.vue"],
     }),
     vueDevTools(),
+    visualizer({
+      open: true, // 直接在浏览器中打开分析报告
+      filename: "stats.html", // 输出文件的名称
+      gzipSize: true, // 显示gzip后的大小
+      brotliSize: true, // 显示brotli压缩后的大小
+    }),
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "lumenUI",
+      fileName: "lumen-ui",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        exports: "named",
+        globals: {
+          vue: "Vue",
+        },
+      },
     },
   },
 });
